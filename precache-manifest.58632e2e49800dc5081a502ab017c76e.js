5748528aea86dd529151d36116793ff5453d369a
Chris Reeve <paradigmsareconstructed@gmail.com>
2019-06-07 00:23:46 -0700

    Updates

---
+++ precache-manifest.58632e2e49800dc5081a502ab017c76e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60790c56e876bcacfb401f3226a5e6df",
+    "url": "/reusable-react-avatar-editor/index.html"
+  },
+  {
+    "revision": "752b11fae6ae4ddfea4e",
+    "url": "/reusable-react-avatar-editor/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "299b2ad407b437a07aca",
+    "url": "/reusable-react-avatar-editor/static/css/main.dfe8a692.chunk.css"
+  },
+  {
+    "revision": "752b11fae6ae4ddfea4e",
+    "url": "/reusable-react-avatar-editor/static/js/2.d31120ee.chunk.js"
+  },
+  {
+    "revision": "299b2ad407b437a07aca",
+    "url": "/reusable-react-avatar-editor/static/js/main.cfa84f4e.chunk.js"
+  },
+  {
+    "revision": "01af5230264feb7cc3d5",
+    "url": "/reusable-react-avatar-editor/static/js/runtime~main.1ef5c219.js"
+  },
+  {
+    "revision": "749a95691d40d6bcf70914fcda7d4894",
+    "url": "/reusable-react-avatar-editor/static/media/carl-sagan.749a9569.jpg"
+  }
+]);
